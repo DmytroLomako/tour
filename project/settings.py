a814ba4935075ba1bf6490389210610add72746d
DmytroLomako <lomakodmytro@gmail.com>
2024-10-12 21:06:06 +0300

    Created tour app

--- project/settings.py
+++ project/settings.py
@@ -5,13 +5,13 @@ import os
 
 
 travel_agency = flask.Flask(
-    import_name= "travel_agency",
+    import_name= "project",
     template_folder= "templates/",
     instance_path= os.path.abspath(__file__ + "/..")
 )
 
 
-travel_agency.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db" 
+travel_agency.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///data.db'
 
 DATABASE = flask_sqlalchemy.SQLAlchemy(app=travel_agency)
 MIGRATE = flask_migrate.Migrate(app=travel_agency, db=DATABASE)
