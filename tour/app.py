68d4557bd64bc92a55905ac4d11e70eacdc4905d
DmytroLomako <lomakodmytro@gmail.com>
2024-11-08 21:33:46 +0200

    add styles

--- tour/app.py
+++ tour/app.py
@@ -3,5 +3,7 @@ import flask
 tour_app = flask.Blueprint(
     name = 'tour',
     import_name = 'tour',
-    template_folder = 'templates'
+    template_folder = 'templates',
+    static_folder= 'static/',
+    static_url_path = "/tour/"
 )
--- user_app/app.py
+++ user_app/app.py
@@ -4,4 +4,6 @@ user_app = flask.Blueprint(
     name = "user",
     import_name = 'user_app',
     template_folder = 'templates',
+    static_folder= "static/",
+    static_url_path= "/user/"
 )
